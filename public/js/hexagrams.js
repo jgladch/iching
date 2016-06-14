80b2f79a36943ccd885c949d28c833532ccb4b05
jgladch <jeffgladchun@gmail.com>
2016-06-14 12:00:07 -0700

    Clean up visibility and scroll issues for commentary card

--- public/js/hexagrams.js
+++ public/js/hexagrams.js
@@ -3,320 +3,512 @@ var hexagrams = [{
   "title": "Initiating",
   "order": 1,
   "sequence": [1, 1, 1, 1, 1, 1],
-  "decision": 'Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright.'
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright.",
+    "decision_commentary": "Vast indeed is the greatness of the Initiating.\n It is the source of all beings And regulates all creations under Heaven.\n Clouds flow and rain falls.\n All beings complete their forms.\n Greatly luminous, from beginning to end.\n Each of the six stages completes itself in its own time, As mounting on six dragons soaring in the sky.\n The way of the Initiating is change and transformation\n So that each being obtains its true nature and destiny\n And the union of great harmony is preserved.\n This is what is favorable and upright.\n The Initiating is high above all beings.\n And thus all countries are united in peace."
+  }
 }, {
   "name": "Kun",
   "title": "Responding",
   "order": 2,
-  "sequence": [0, 0, 0, 0, 0, 0]
+  "sequence": [0, 0, 0, 0, 0, 0],
+  "commentary": {
+    "decision": "Responding.\n Sublimely prosperous and smooth.\n Favorable with a mare’s steadfastness.\n Superior person has somewhere to go.\n Predetermining, loses: Following, obtains a master.\n Favorable in the southwest: Gets friends.\n In the northeast: Lose friends.\n Be composed and content.\n Being steadfast and upright: good fortune."
+  }
 }, {
   "name": "Zhun",
   "title": "Beginning",
   "order": 3,
-  "sequence": [1, 0, 0, 0, 1, 0]
+  "sequence": [1, 0, 0, 0, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Meng",
   "title": "Childhood",
   "order": 4,
-  "sequence": [0, 1, 0, 0, 0, 1]
+  "sequence": [0, 1, 0, 0, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Xü",
   "title": "Needing",
   "order": 5,
-  "sequence": [1, 1, 1, 0, 1, 0]
+  "sequence": [1, 1, 1, 0, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Song",
   "title": "Contention",
   "order": 6,
-  "sequence": [0, 1, 0, 1, 1, 1]
+  "sequence": [0, 1, 0, 1, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Shi",
   "title": "Multitude",
   "order": 7,
-  "sequence": [0, 1, 0, 0, 0, 0]
+  "sequence": [0, 1, 0, 0, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Bi",
   "title": "Union",
   "order": 8,
-  "sequence": [0, 0, 0, 0, 1, 0]
+  "sequence": [0, 0, 0, 0, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Xiao Xü",
   "title": "Little Accumulation",
   "order": 9,
-  "sequence": [1, 1, 1, 0, 1, 1]
+  "sequence": [1, 1, 1, 0, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Lü",
   "title": "Fulfillment",
   "order": 10,
-  "sequence": [1, 1, 0, 1, 1, 1]
+  "sequence": [1, 1, 0, 1, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Tai",
   "title": "Advance",
   "order": 11,
-  "sequence": [1, 1, 1, 0, 0, 0]
+  "sequence": [1, 1, 1, 0, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Pi",
   "title": "Hinderance",
   "order": 12,
-  "sequence": [0, 0, 0, 1, 1, 1]
+  "sequence": [0, 0, 0, 1, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Tong Ren",
   "title": "Seeking Harmony",
   "order": 13,
-  "sequence": [1, 0, 1, 1, 1, 1]
+  "sequence": [1, 0, 1, 1, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Da You",
   "title": "Great Harvest",
   "order": 14,
-  "sequence": [1, 1, 1, 1, 0, 1]
+  "sequence": [1, 1, 1, 1, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Qian",
   "title": "Humbleness",
   "order": 15,
-  "sequence": [0, 0, 1, 0, 0, 0]
+  "sequence": [0, 0, 1, 0, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Yü",
   "title": "Delight",
   "order": 16,
-  "sequence": [0, 0, 0, 1, 0, 0]
+  "sequence": [0, 0, 0, 1, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Sui",
   "title": "Following",
   "order": 17,
-  "sequence": [1, 0, 0, 1, 1, 0]
+  "sequence": [1, 0, 0, 1, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Gu",
   "title": "Remedying",
   "order": 18,
-  "sequence": [0, 1, 1, 0, 0, 1]
+  "sequence": [0, 1, 1, 0, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Lin",
   "title": "Approaching",
   "order": 19,
-  "sequence": [1, 1, 0, 0, 0, 0]
+  "sequence": [1, 1, 0, 0, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Guan",
   "title": "Watching",
   "order": 20,
-  "sequence": [0, 0, 0, 0, 1, 1]
+  "sequence": [0, 0, 0, 0, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Shi He",
   "title": "Eradicating",
   "order": 21,
-  "sequence": [1, 0, 0, 1, 0, 1]
+  "sequence": [1, 0, 0, 1, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Bi",
   "title": "Adorning",
   "order": 22,
-  "sequence": [1, 0, 1, 0, 0, 1]
+  "sequence": [1, 0, 1, 0, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Bo",
   "title": "Falling Away",
   "order": 23,
-  "sequence": [0, 0, 0, 0, 0, 1]
+  "sequence": [0, 0, 0, 0, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Fu",
   "title": "Turning Back",
   "order": 24,
-  "sequence": [1, 0, 0, 0, 0, 0]
+  "sequence": [1, 0, 0, 0, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Wu Wang",
   "title": "Without Falsehood",
   "order": 25,
-  "sequence": [1, 0, 0, 1, 1, 1]
+  "sequence": [1, 0, 0, 1, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Da Xü",
   "title": "Great Accumulation",
   "order": 26,
-  "sequence": [1, 1, 1, 0, 0, 1]
+  "sequence": [1, 1, 1, 0, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Yi",
   "title": "Nourishing",
   "order": 27,
-  "sequence": [1, 0, 0, 0, 0, 1]
+  "sequence": [1, 0, 0, 0, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Da Guo",
   "title": "Great Exceeding",
   "order": 28,
-  "sequence": [0, 1, 1, 1, 1, 0]
+  "sequence": [0, 1, 1, 1, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Kan",
   "title": "Darkness",
   "order": 29,
-  "sequence": [0, 1, 0, 0, 1, 0]
+  "sequence": [0, 1, 0, 0, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Li",
   "title": "Brightness",
   "order": 30,
-  "sequence": [1, 0, 1, 1, 0, 1]
+  "sequence": [1, 0, 1, 1, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Xian",
   "title": "Mutual Influence",
   "order": 31,
-  "sequence": [0, 0, 1, 1, 1, 0]
+  "sequence": [0, 0, 1, 1, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Heng",
   "title": "Long Lasting",
   "order": 32,
-  "sequence": [0, 1, 1, 1, 0, 0]
+  "sequence": [0, 1, 1, 1, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Dun",
   "title": "Retreat",
   "order": 33,
-  "sequence": [0, 0, 1, 1, 1, 1]
+  "sequence": [0, 0, 1, 1, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Da Zhuang",
   "title": "Great Strength",
   "order": 34,
-  "sequence": [1, 1, 1, 1, 0, 0]
+  "sequence": [1, 1, 1, 1, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Jing",
   "title": "Proceeding Forward",
   "order": 35,
-  "sequence": [0, 0, 0, 1, 0, 1]
+  "sequence": [0, 0, 0, 1, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Ming Yi",
   "title": "Brilliance Injured",
   "order": 36,
-  "sequence": [1, 0, 1, 0, 0, 0]
+  "sequence": [1, 0, 1, 0, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Jia Ren",
   "title": "Household",
   "order": 37,
-  "sequence": [1, 0, 1, 0, 1, 1]
+  "sequence": [1, 0, 1, 0, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Kui",
   "title": "Diversity",
   "order": 38,
-  "sequence": [1, 1, 0, 1, 0, 1]
+  "sequence": [1, 1, 0, 1, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Jian",
   "title": "Hardship",
   "order": 39,
-  "sequence": [0, 0, 1, 0, 1, 0]
+  "sequence": [0, 0, 1, 0, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Jie",
   "title": "Relief",
   "order": 40,
-  "sequence": [0, 1, 0, 1, 0, 0]
+  "sequence": [0, 1, 0, 1, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Sun",
   "title": "Decreasing",
   "order": 41,
-  "sequence": [1, 1, 0, 0, 0, 1]
+  "sequence": [1, 1, 0, 0, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Yi",
   "title": "Increasing",
   "order": 42,
-  "sequence": [1, 0, 0, 0, 1, 1]
+  "sequence": [1, 0, 0, 0, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Guai",
   "title": "Eliminating",
   "order": 43,
-  "sequence": [1, 1, 1, 1, 1, 0]
+  "sequence": [1, 1, 1, 1, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Gou",
   "title": "Encountering",
   "order": 44,
-  "sequence": [0, 1, 1, 1, 1, 1]
+  "sequence": [0, 1, 1, 1, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Cui",
   "title": "Bringing Together",
   "order": 45,
-  "sequence": [0, 0, 0, 1, 1, 0]
+  "sequence": [0, 0, 0, 1, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Sheng",
   "title": "Growing Upward",
   "order": 46,
-  "sequence": [0, 1, 1, 0, 0, 0]
+  "sequence": [0, 1, 1, 0, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Kun",
   "title": "Exhausting",
   "order": 47,
-  "sequence": [0, 1, 0, 1, 1, 0]
+  "sequence": [0, 1, 0, 1, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Jing",
   "title": "Replenishing",
   "order": 48,
-  "sequence": [0, 1, 1, 0, 1, 0]
+  "sequence": [0, 1, 1, 0, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Ge",
   "title": "Abolishing the Old",
   "order": 49,
-  "sequence": [1, 0, 1, 1, 1, 0]
+  "sequence": [1, 0, 1, 1, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Ding",
   "title": "Establishing the New",
   "order": 50,
-  "sequence": [0, 1, 1, 1, 0, 1]
+  "sequence": [0, 1, 1, 1, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Zhen",
   "title": "Taking Action",
   "order": 51,
-  "sequence": [1, 0, 0, 1, 0, 0]
+  "sequence": [1, 0, 0, 1, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Gen",
   "title": "Keeping Still",
   "order": 52,
-  "sequence": [0, 0, 1, 0, 0, 1]
+  "sequence": [0, 0, 1, 0, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Jian",
   "title": "Developing Gradually",
   "order": 53,
-  "sequence": [0, 0, 1, 0, 1, 1]
+  "sequence": [0, 0, 1, 0, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Gui Mei",
   "title": "Marrying Maiden",
   "order": 54,
-  "sequence": [1, 1, 0, 1, 0, 0]
+  "sequence": [1, 1, 0, 1, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Feng",
   "title": "Abundance",
   "order": 55,
-  "sequence": [1, 0, 1, 1, 0, 0]
+  "sequence": [1, 0, 1, 1, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Lü",
   "title": "Traveling",
   "order": 56,
-  "sequence": [0, 0, 1, 1, 0, 1]
+  "sequence": [0, 0, 1, 1, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Xun",
   "title": "Proceeding Humbly",
   "order": 57,
-  "sequence": [0, 1, 1, 0, 1, 1]
+  "sequence": [0, 1, 1, 0, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Dui",
   "title": "Joyful",
   "order": 58,
-  "sequence": [1, 1, 0, 1, 1, 0]
+  "sequence": [1, 1, 0, 1, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Huan",
   "title": "Dispersing",
   "order": 59,
-  "sequence": [0, 1, 0, 0, 1, 1]
+  "sequence": [0, 1, 0, 0, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Jie",
   "title": "Restricting",
   "order": 60,
-  "sequence": [1, 1, 0, 0, 1, 0]
+  "sequence": [1, 1, 0, 0, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Zhong Fu",
   "title": "Innermost Sincerity",
   "order": 61,
-  "sequence": [1, 1, 0, 0, 1, 1]
+  "sequence": [1, 1, 0, 0, 1, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Xiao Guo",
   "title": "Little Exceeding",
   "order": 62,
-  "sequence": [0, 0, 1, 1, 0, 0]
+  "sequence": [0, 0, 1, 1, 0, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Ji Ji",
   "title": "Already Fulfilled",
   "order": 63,
-  "sequence": [1, 0, 1, 0, 1, 0]
+  "sequence": [1, 0, 1, 0, 1, 0],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }, {
   "name": "Wei Ji",
   "title": "Not Yet Fulfilled",
   "order": 64,
-  "sequence": [0, 1, 0, 1, 0, 1]
+  "sequence": [0, 1, 0, 1, 0, 1],
+  "commentary": {
+    "decision": "Initiating.\n Sublime and initiative.\n Prosperous and smooth.\n Favorable and beneficial.\n Steadfast and upright."
+  }
 }];
