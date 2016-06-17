76eb89c1a256398d22c55fa1c480c2c5a82fb65b
jgladch <jeffgladchun@gmail.com>
2016-06-17 09:31:58 -0700

    Add basic tutorial mode for first time visitors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
 import path from 'path';
+import session from 'cookie-session';
 import hexagrams from '../bin/hexagrams.json';
 
 const app = express();
@@ -20,11 +21,30 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, '../', 'public')));
 
+app.use(session({
+  secret: 'ABCDEFG',
+  cookie: {
+    maxAge: 604800000
+  },
+  overwrite: false
+}));
+
+app.use((req, res, next) => {
+  if (!req.session.visitCount) {
+    req.session.visitCount = 1;
+  } else {
+    req.session.visitCount++;
+  }
+
+  next();
+});
+
 app.set('port', process.env.PORT || 3030);
 
 app.get('/', (req, res, next) => {
   return res.render('index', {
     hexagrams: hexagrams,
+    visits: req.session && req.session.visitCount,
     breathing: false
   });
 });
